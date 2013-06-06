1c437071f5c97758ff44ca02a140bc5b2bc8520c
Albert <Albertpa@gmail.com>
2013-06-06 19:22:30 +0200

    Initial Commit

---
+++ TablaMVC/APRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  APRAppDelegate.h
+//  TablaMVC
+//
+//  Created by Albert  on 06/06/13.
+//  Copyright (c) 2013 Albert Pages. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TablaMVC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TablaMVC
+//
+//  Created by Albert  on 06/06/13.
+//  Copyright (c) 2013 Albert Pages. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "APRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([APRAppDelegate class]));
+    }
+}
